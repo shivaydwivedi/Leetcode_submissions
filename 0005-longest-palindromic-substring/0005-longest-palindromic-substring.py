dd8ab588d50bcd12f861f34a7c53ec4732c1e9ad
shivaydwivedi <167169823+shivaydwivedi@users.noreply.github.com>
2025-11-01 23:56:32 +0530

    Time: 266 ms (67.2%), Space: 17.7 MB (83.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,15 +1,32 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # max length 
-        max_len = 0
-        # longest substring
-        longest = " "
-        n = len(s)
-        # iterate through the string 
-        for i in range(n):
-            for j in range(i, n):
-                substring = s[i:j+1]
-                if substring == substring[::-1] and len(substring) > max_len:
-                    max_len = len(substring)
-                    longest = substring
-        return longest
+        if not s:
+            return ""
+
+        start = 0
+        end = 0
+
+        # Helper function to expand around a given center
+        def expandFromCenter(left: int, right: int) -> int:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            # Length of the palindrome found
+            return right - left - 1
+
+        for i in range(len(s)):
+            # Case 1: Odd-length palindrome (center at s[i])
+            len1 = expandFromCenter(i, i)
+            # Case 2: Even-length palindrome (center between s[i] and s[i+1])
+            len2 = expandFromCenter(i, i + 1)
+
+            # Take the longer palindrome length
+            max_len = max(len1, len2)
+
+            # If we found a longer palindrome, update start and end
+            if max_len > (end - start):
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        # Return the longest palindromic substring
+        return s[start:end + 1]
