841722adc8da483ad96d7f0729a94e9abc91c972
shivaydwivedi <167169823+shivaydwivedi@users.noreply.github.com>
2026-01-22 23:29:35 +0530

    Time: 0 ms (100%), Space: 20.4 MB (26.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
+        # sum of n natural numbers
         nat_sum = (n*(n+1))//2
+        # actual sum of numbers
         act_sum = sum(nums)
         return nat_sum - act_sum
 
