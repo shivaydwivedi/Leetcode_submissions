6bb93e2532c3c611a6737c593e35d2f87976ffdd
shivaydwivedi <167169823+shivaydwivedi@users.noreply.github.com>
2026-01-30 00:49:04 +0530

    Time: 4 ms (10.82%), Space: 20.4 MB (24.52%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,29 +6,9 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # counteer to track max depth
-        max_depth = 0
+        if root is None:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
 
-        # Traverse the tree 
-        def traverse(node, current_depth):
-            
-            nonlocal max_depth
-            # check if tree is empty
-            if node is None:
-                return
-            
-
-            # if tree is not empty
-            current_depth += 1
-
-            # if you find leaf node
-            if node.left is None and node.right is None:
-                max_depth = max(max_depth, current_depth)
-            
-            # find depth of both sides
-            traverse(node.right, current_depth)
-            traverse(node.left, current_depth)
-        
-        
-        traverse(root, 0)
-        return max_depth
+        return 1+max(left_depth, right_depth)
