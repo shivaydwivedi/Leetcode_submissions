4d858e1d344f2dd00b66d19ea9a0a101243b0eaa
shivaydwivedi <167169823+shivaydwivedi@users.noreply.github.com>
2026-03-27 00:16:28 +0530

    Time: 11 ms (18.37%), Space: 20.5 MB (47.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,16 +1,14 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        # length of the array
+        
         n = len(nums)
+
         # slow pointer
-        slow = 0
-        # scan the array with fast
-        for fast in range(0,n):
-            # if a non-zero number
-            if nums[fast] != 0:
-                # swap it with nums[slow]
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                # increment the slow pointer
-                slow += 1
-        # return the output
+        s = 0
+
+        # move through the array
+        for f in range(0,n):
+            if nums[f] != 0:
+                nums[s], nums[f] = nums[f], nums[s]
+                s += 1
         return nums
