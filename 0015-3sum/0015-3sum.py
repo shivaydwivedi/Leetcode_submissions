b51985d0cfa764d05435ba66165cac14ae1e63fa
shivaydwivedi <167169823+shivaydwivedi@users.noreply.github.com>
2026-03-30 22:47:19 +0530

    Time: 637 ms (56.24%), Space: 22.2 MB (66.86%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,40 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # length of array
-        n = len(nums)
-        # sort the array
+        # sort the array 
         nums.sort()
-        # create tre result array
+        # result array 
         res = []
-        # iterate over the array
-        for i in range(n):
-            # remove duplicates of i
+        
+        # set one number and prevent duplicates
+        for i in range(len(nums)):
+            # skip duplicates
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            # left pointer
-            l = i+1
-            # right pointer
-            r = n-1
+        # define pointers
+            l , r = i+1, len(nums)-1  
+        # iterate over the array 
             while l < r:
-                # calculate the total
                 total = nums[i] + nums[l] + nums[r]
-                if total < 0:
-                    l += 1
-                elif total > 0:
-                    r -= 1
-                else :
-                    # add the triplet to the result array 
-                    res.append([nums[i] , nums[l] , nums[r]])
-                    l += 1
-                    r -= 1
-                    # remove duplicates of l
-                    while l < r and nums[l] == nums[l-1]:
+
+            # total = 0 condition
+                if total == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+
+                    while l<r and nums[l] == nums[l+1]:
                         l += 1
-                    # remove duplicates of r
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1
+                    while l > r and nums[r] == nums[r-1]:
+                        r += 1
+                
+                    l += 1
+                    r -= 1
+            
+            # total < 0 
+                elif total < 0:
+                    l += 1
+            # total > 0
+                else: 
+                    r -= 1
+
         return res
+
+
